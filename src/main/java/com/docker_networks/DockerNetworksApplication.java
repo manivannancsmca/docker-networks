374da42def6767b4da11907b8bf1c76e831e9270
manivannancsmca <manivannancsmca@gmail.com>
2026-07-21 23:16:27 +0530

    first commit

---
+++ src/main/java/com/docker_networks/DockerNetworksApplication.java
@@ -0,0 +1,13 @@
+package com.docker_networks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerNetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerNetworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docker_networks/DockerNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker_networks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerNetworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
